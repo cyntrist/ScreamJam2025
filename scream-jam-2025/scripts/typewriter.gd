ee2ece668846bb7a226ac951a2955ed84e97faea
Cynthia Tristán <cyntrist@ucm.es>
2025-10-17 01:55:36 +0200

    hecho

---
+++ scream-jam-2025/scripts/typewriter.gd
@@ -0,0 +1,9 @@
+extends RichTextLabel
+
+
+#var tween: Tween = create_tween()
+#tween.tween_property(self, "visible_ratio", 1.0, 2.0).from(0.0)
+#
+#variables for typing speed, total characters, and the current character count or time elapsed. 
+#
+#check if typing is active. If so, add delta time to a timer or directly increment the visible_characters based on text_speed and delta. 
