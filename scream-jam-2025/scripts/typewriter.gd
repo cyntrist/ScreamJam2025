6b4e4a07951c76d96f52a095c057fe08c14f2494
Cynthia Tristán <cyntrist@ucm.es>
2025-10-19 02:24:28 +0200

    no mola tanto

--- scream-jam-2025/scripts/typewriter.gd
+++ scream-jam-2025/scripts/typewriter.gd
@@ -7,8 +7,10 @@ extends RichTextLabel
 #variables for typing speed, total characters, and the current character count or time elapsed. 
 #
 #check if typing is active. If so, add delta time to a timer or directly increment the visible_characters based on text_speed and delta. 
-
 @export var chars_per_second: float = 30.0
+@export var hover_color : Color = Color(0.0, 0.173, 0.737, 1.0)
+@export var normal_color : Color = Color(1.0, 1.0, 1.0, 1.0)
+@export var hover_speed = 0.1
 var text_displayed: float = 0
 var text_length = 0;
 
@@ -30,10 +32,23 @@ func iniciar_dialogo(ind):
 
 
 func _on_mouse_entered() -> void:
-	self.add_theme_color_override("default_color", Color(0.0, 0.173, 0.737, 1.0)) 
+	#self.add_theme_color_override("default_color", Color(0.0, 0.173, 0.737, 1.0)) 
+	
+	create_tween().tween_method(
+		func(color): add_theme_color_override("default_color", color),
+		get_theme_color("default_color"),
+		hover_color,
+		hover_speed
+	)
 	pass # Replace with function body.
 
 
 func _on_mouse_exited() -> void:
-	self.add_theme_color_override("default_color", Color(1.0, 1.0, 1.0, 1.0)) 
+	#self.add_theme_color_override("default_color", Color(1.0, 1.0, 1.0, 1.0)) 
+	create_tween().tween_method(
+		func(color): add_theme_color_override("default_color", color),
+		get_theme_color("default_color"),
+		normal_color,
+		hover_speed
+	)
 	pass # Replace with function body.
